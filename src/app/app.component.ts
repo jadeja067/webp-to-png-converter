4f6f1bd1a4bb017bf1a39462ead18b1512ae0764
Kuldipsinh Jadeja <69837239+jadeja067@users.noreply.github.com>
2024-07-23 12:19:06 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,5 +47,40 @@ export class AppComponent {
     const pngFile = new File([blob], 'converted.png', { type: 'image/png' });
     return pngFile;
   }
+
+  // second option using canvas 
+  /*
+    private async convertWebpToPng(webpDataUrl: string): Promise<File> {
+  const img = new Image();
+  img.src = webpDataUrl;
+  const imageLoadPromise = new Promise<void>((resolve, reject) => {
+    img.onload = () => resolve();
+    img.onerror = () => reject(new Error('Failed to load the image'));
+  });
+
+  await imageLoadPromise;
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    throw new Error('Failed to get canvas context');
+  }
+
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  ctx.drawImage(img, 0, 0);
+
+  const pngDataUrl = canvas.toDataURL('image/png');
+
+  const response = await fetch(pngDataUrl);
+  const blob = await response.blob();
+
+  const pngFile = new File([blob], 'converted.png', { type: 'image/png' });
+  
+  return pngFile;
+}
+  */
   submit() {}
 }
